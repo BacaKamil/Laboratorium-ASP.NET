fa47bf28362720f2ba8e47c0ae814ed0572072b8
BacaKamil <kamil.baca@outlook.com>
2023-10-23 01:16:04 +0200

    Lab3_Reservation

---
+++ Laboratorium 3 - App/Controllers/ReservationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium_3___App.Controllers
+{
+    public class ReservationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Laboratorium 3 - App/Models/Reservation.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laboratorium_3___App.Models
+{
+    public class Reservation
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Data { get; set; }
+        public string Miasto { get; set; }
+        public string Adres { get; set; }
+        public string Pokoj { get; set; }
+        public string Wlasciciel { get; set; }
+        public decimal Cena { get; set; }
+    }
+}
